fc943f2833efef66fa286acdac4ba549c725f486
atiqur <atiqur@shaficonsultancy.com>
2019-05-21 21:59:09 +0600

    #initial commit.

---
+++ src/Pdf.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by Md. Atiqur Rahman. <atiq.cse.cu0506.su@gmail.com, atiqur@shaficonsultancy.com>
+ * Date: 5/21/2019
+ * Time: 9:45 PM
+ */
+
+namespace Atiq\APDF;
+
+
+class Pdf {
+
+
+    /**
+     * @author Md. Atiqur Rahman <atiq.cse.cu0506.su@gmail.com, atiqur@shaficonsultancy.com>
+     * @return string
+     */
+    public function hi() {
+
+        return 'Hi, how are you doing?';
+    }
+}
